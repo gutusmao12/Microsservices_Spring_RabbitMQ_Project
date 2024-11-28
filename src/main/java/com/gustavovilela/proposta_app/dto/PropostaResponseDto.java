4d498efbb51f1e7985a089f84f29cb4800bf82f6
Gustavo Vilela <gutusmao12@hotmail.com>
2024-11-27 21:15:37 -0300

    feat: mapping PropostaRequest and PropostaResponse

--- src/main/java/com/gustavovilela/proposta_app/dto/PropostaResponseDto.java
+++ src/main/java/com/gustavovilela/proposta_app/dto/PropostaResponseDto.java
@@ -1,4 +1,33 @@
 package com.gustavovilela.proposta_app.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class PropostaResponseDto {
+
+    private Long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String cpf;
+
+    private String telefone;
+
+    private Double renda;
+
+    private Double valorSolicitado;
+
+    private int prazoPagamento;
+
+    private Boolean aprovado;
+
+    private String observacao;
 }
