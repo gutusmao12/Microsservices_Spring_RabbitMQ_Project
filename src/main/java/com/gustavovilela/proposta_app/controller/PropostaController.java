1296fe42ef77f9226cf26f266276d8e90f6e3296
Gustavo Vilela <gutusmao12@hotmail.com>
2024-11-27 21:36:44 -0300

    feat: instantiating PropostaService with Spring

--- src/main/java/com/gustavovilela/proposta_app/controller/PropostaController.java
+++ src/main/java/com/gustavovilela/proposta_app/controller/PropostaController.java
@@ -3,12 +3,14 @@ package com.gustavovilela.proposta_app.controller;
 import com.gustavovilela.proposta_app.dto.PropostaRequestDto;
 import com.gustavovilela.proposta_app.dto.PropostaResponseDto;
 import com.gustavovilela.proposta_app.service.PropostaService;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/proposta")
 public class PropostaController {
--- src/main/java/com/gustavovilela/proposta_app/service/PropostaService.java
+++ src/main/java/com/gustavovilela/proposta_app/service/PropostaService.java
@@ -2,7 +2,9 @@ package com.gustavovilela.proposta_app.service;
 
 import com.gustavovilela.proposta_app.dto.PropostaRequestDto;
 import com.gustavovilela.proposta_app.dto.PropostaResponseDto;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PropostaService {
 
     public PropostaResponseDto criar(PropostaRequestDto requestDto) {
