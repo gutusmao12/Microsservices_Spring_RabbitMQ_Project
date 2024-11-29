3b7d56aa2f09dc84fbc1a8b7c5d65051e308f63e
Gustavo Vilela <gutusmao12@hotmail.com>
2024-11-29 19:30:19 -0300

    feat: Starting mapping

--- src/main/java/com/gustavovilela/proposta_app/service/PropostaService.java
+++ src/main/java/com/gustavovilela/proposta_app/service/PropostaService.java
@@ -2,10 +2,15 @@ package com.gustavovilela.proposta_app.service;
 
 import com.gustavovilela.proposta_app.dto.PropostaRequestDto;
 import com.gustavovilela.proposta_app.dto.PropostaResponseDto;
+import com.gustavovilela.proposta_app.repository.PropostaRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class PropostaService {
+
+    private PropostaRepository propostaRepository;
 
     public PropostaResponseDto criar(PropostaRequestDto requestDto) {
         return null;
