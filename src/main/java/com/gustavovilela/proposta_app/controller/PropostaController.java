7af456a56a3bbb1b77cca12aac866cf2e4423802
Gustavo Vilela <gutusmao12@hotmail.com>
2024-11-29 20:05:16 -0300

    feat: Restfull patterns

--- src/main/java/com/gustavovilela/proposta_app/controller/PropostaController.java
+++ src/main/java/com/gustavovilela/proposta_app/controller/PropostaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @AllArgsConstructor
 @RestController
@@ -20,6 +21,10 @@ public class PropostaController {
     @PostMapping
     public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto) {
         PropostaResponseDto response =  propostaService.criar(requestDto);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.getId())
+                .toUri())
+                .body(response);
     }
 }
