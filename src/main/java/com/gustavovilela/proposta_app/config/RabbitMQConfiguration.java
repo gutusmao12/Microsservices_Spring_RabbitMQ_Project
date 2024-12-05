03f23f2a42f41e0db36b5fe05fc38cd94d43b0a4
Gustavo Vilela <gutusmao12@hotmail.com>
2024-12-05 18:41:39 -0300

    feat: Sending first message and creating our exchange

--- src/main/java/com/gustavovilela/proposta_app/config/RabbitMQConfiguration.java
+++ src/main/java/com/gustavovilela/proposta_app/config/RabbitMQConfiguration.java
@@ -1,13 +1,23 @@
 package com.gustavovilela.proposta_app.config;
 
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
+
+    @Value("${rabbitmq.propostapendente.exchange}")
+    private String exchange;
 
     @Bean
     public Queue criarFilaPropostaPendenteMsAnaliseCredito() {
@@ -27,5 +37,46 @@ public class RabbitMQConfiguration {
     @Bean
     public Queue criarFilaPropostaConcluidaMsNotificacao() {
         return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
+    }
+
+    @Bean
+    public RabbitAdmin criarRabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> inicializarAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
+    public FanoutExchange criarFanoutExchangePropostaPendente() {
+        return ExchangeBuilder.fanoutExchange(exchange).build();
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendenteMsAnaliseCredito() {
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsAnaliseCredito()).
+                to(criarFanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public Binding criarBindingPropostaPendenteMsNotificacao() {
+        return BindingBuilder.bind(criarFilaPropostaPendenteMsNotificacao()).
+                to(criarFanoutExchangePropostaPendente());
+    }
+
+    @Bean
+    public MessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+
+        return rabbitTemplate;
     }
 }
--- src/main/java/com/gustavovilela/proposta_app/service/PropostaService.java
+++ src/main/java/com/gustavovilela/proposta_app/service/PropostaService.java
@@ -6,20 +6,36 @@ import com.gustavovilela.proposta_app.entity.Proposta;
 import com.gustavovilela.proposta_app.mapper.PropostaMapper;
 import com.gustavovilela.proposta_app.repository.PropostaRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@AllArgsConstructor
 @Service
 public class PropostaService {
 
     private PropostaRepository propostaRepository;
 
+    private NotificacaoService notificacaoService;
+
+    private String exchange;
+
+    public PropostaService(NotificacaoService notificacaoService,
+                           PropostaRepository propostaRepository,
+                           @Value("${rabbitmq.propostapendente.exchange}") String exchange) {
+        this.notificacaoService = notificacaoService;
+        this.propostaRepository = propostaRepository;
+        this.exchange = exchange;
+    }
+
     public PropostaResponseDto criar(PropostaRequestDto requestDto) {
         Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(requestDto);
         propostaRepository.save(proposta);
-        return PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+
+        PropostaResponseDto response = PropostaMapper.INSTANCE.convertEntityToDto(proposta);
+        notificacaoService.notificar(response, exchange);
+
+        return response;
     }
 
     public List<PropostaResponseDto> obterProposta() {
